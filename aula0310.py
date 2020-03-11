ea1ca3992e6dd50a8f5df612b5b46ddcf3d73578
julian-julian <54966961+julian-julian@users.noreply.github.com>
2020-03-11 01:37:57 +0000

    Teste file
    teste

---
+++ aula0310.py
@@ -0,0 +1,32 @@
+
+
+
+n1 = 2
+n2 = 3
+soma = n1+n2
+
+if(soma>6):
+    print("Maior que 6")
+    print ("Menor que 6")
+
+    print("maior que 6")
+
+for i in range (5,10):
+    print(i)
+    print (soma)
+
+
+for item in[3,4,5,6,7]:
+    print (item)
+
+
+contador = 0
+while contador < 5:
+    print(contador)
+    contador = contador + 1
+
+idade = 18
+if idade >= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
